a1b82f89fb01f6f45fc097d09f03fa17aa92c2a3
worstnightmares <knightplugg@gmail.com>
2026-02-05 10:58:43 +0300

    Update Signature.lua

--- Signature.lua
+++ Signature.lua
@@ -3427,7 +3427,7 @@ function SignatureUI:_build()
 
 		local bar = mk("Frame", {
 			Position = UDim2.new(0, 10, 0, 12),
-			Size = UDim2.new(0, 4, 1, -45),
+			Size = UDim2.new(0, 4, 1, -40),
 			BackgroundColor3 = color,
 			BackgroundTransparency = 0.06,
 			BorderSizePixel = 0,
